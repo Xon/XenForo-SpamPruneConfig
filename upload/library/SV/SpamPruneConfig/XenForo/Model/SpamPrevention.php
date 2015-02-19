493091167f4268c4f997ae153c3a0c9968175f0a
Xon <tazipper1@hotmail.com>
2015-02-19 20:28:44 +0800

    Tweak queries

--- upload/library/SV/SpamPruneConfig/XenForo/Model/SpamPrevention.php
+++ upload/library/SV/SpamPruneConfig/XenForo/Model/SpamPrevention.php
@@ -7,36 +7,25 @@ class SV_SpamPruneConfig_XenForo_Model_SpamPrevention extends XFCP_SV_SpamPruneC
         {
             $cutOff = XenForo_Application::get('options')->SV_SpamPruneConfig_Prune_Default * 86400;
         }
+
         if ($cutOff > 0)
         {
-            $this->_getDb()->query("delete from xf_spam_trigger_log where content_type <> 'user' and log_date < ? ", array($cutOff));
+            $this->_getDb()->query("delete from xf_spam_trigger_log where content_type <> 'user' and log_date < ? ", array(XenForo_Application::$time - $cutOff));
         }
-        
         $cutOff = XenForo_Application::get('options')->SV_SpamPruneConfig_Prune_User_Rejected;
         if ($cutOff > 0)
         {
-            $this->_getDb()->query("delete from xf_spam_trigger_log where content_type = ? and result = ? and log_date < ? ", array('user', self::RESULT_DENIED, $cutOff * 86400));
+            $this->_getDb()->query("delete from xf_spam_trigger_log where content_type = ? and result = ? and log_date < ? ", array('user', self::RESULT_DENIED, XenForo_Application::$time - $cutOff * 86400));
         }
         $cutOff = XenForo_Application::get('options')->SV_SpamPruneConfig_Prune_User_Moderated ;
         if ($cutOff > 0)
         {
-            $this->_getDb()->query("delete from xf_spam_trigger_log where content_type = ? and result = ? and log_date < ? ", array('user', self::RESULT_MODERATED, $cutOff * 86400));
+            $this->_getDb()->query("delete from xf_spam_trigger_log where content_type = ? and result = ? and log_date < ? ", array('user', self::RESULT_MODERATED, XenForo_Application::$time - $cutOff * 86400));
         }
         $cutOff = XenForo_Application::get('options')->SV_SpamPruneConfig_Prune_User_Allowed ;
         if ($cutOff > 0)
         {
-            $this->_getDb()->query("delete from xf_spam_trigger_log where content_type = ? and result = ? and log_date < ? ", array('user', self::RESULT_ALLOWED, $cutOff * 86400));
+            $this->_getDb()->query("delete from xf_spam_trigger_log where content_type = ? and result = ? and log_date < ? ", array('user', self::RESULT_ALLOWED, XenForo_Application::$time - $cutOff * 86400));
         }
     }
-/*
-    public function cleanupContentSpamCheck($cutOff = null)
-    {
-        if ($cutOff === null)
-        {
-            $cutOff = XenForo_Application::get('options')->SV_SpamPruneConfig_ContentSpamCheckPruneDays * 86400;
-        }
-
-        $this->_getDb()->delete('xf_content_spam_cache', 'insert_date < ' . intval($cutOff));
-    }
-*/
 }
